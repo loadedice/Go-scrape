819a35f43db2f350c7dde9d93ecaa157464b2096
joelina <joelina@users.noreply.github.com>
2013-09-23 14:17:08 +1000

    New script, go-deeper.py

---
+++ go-deeper.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import time
+
+with open(sys.argv[2], "rt") as file:
+    for line in file:
+        os.system("./scrape"+sys.argv[1]+" "+line)
+        time.sleep(1) #sleep between so you can quit and so it doesn't spam the site with requests. You can delete this line if you know the site won't mind, but beware you'll need to hold down control an c for each of the go programs because of the way that they run
